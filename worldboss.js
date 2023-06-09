66c3e77ad94dcb7abf4e019bef827f9d4365f5ef
Mitsuuki <123576459+Mitsuuki@users.noreply.github.com>
2023-06-09 21:38:51 +0800

    Add files via upload

--- worldboss.js
+++ worldboss.js
@@ -67,14 +67,6 @@ var images = [
         url: "images/setekh wenut.webp",
         text: "Setekh Wenut"
       },
-      {
-        url: "images/setekh wenut.webp",
-        text: "Setekh Wenut"
-      },
-      {
-        url: "images/eulsNani.png",
-        text: "Apep"
-      },
     // Add more image objects here
   ];
   
