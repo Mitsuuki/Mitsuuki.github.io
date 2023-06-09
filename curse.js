45975c28f28bf1571522e373dcec872e6999c154
Mitsuuki <123576459+Mitsuuki@users.noreply.github.com>
2023-06-09 22:15:35 +0800

    Add files via upload

--- curse.js
+++ curse.js
@@ -2,8 +2,6 @@ var textOutputModal = document.getElementById("text-output-modal");
 var textOutput = document.getElementById("text-output");
 
 var texts = [
-  "Surprise Blessing: Obtain 3 additional skips for boss.",
-  "Surprise Blessing: Obtain 3 skips for curses.",
   "Surprise blessing: Euls must participate in the team fight + reroll for curse. - <3",
   "Starter Character vs Bathysmal - LetmeSleep",
   "Regular Banning",
